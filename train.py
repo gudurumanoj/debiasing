99538c76768b928c24509430b6e6e6401aaff932
gudurumanoj <gudurumanoj271102@gmail.com>
2024-02-01 11:06:06 +0000

    cleaned train.py

--- train.py
+++ train.py
@@ -225,6 +225,7 @@ class learner(nn.Module):
 
         self.criteria = BCEloss()
         self.trainInfoList = []
+        self.modeltrainvalinfo = []
         self.valInfoList = []
 
         self.weights = torch.rand(80).cuda() ## just initialising it randomly for the first round
@@ -235,7 +236,7 @@ class learner(nn.Module):
 
     def forward(self):
         for epoch in range(self.epochs):
-            # print(self.weights)
+            print(self.weights)
             if epoch > self.stop_epoch:
                 break
             
@@ -258,47 +259,82 @@ class learner(nn.Module):
                 loss.backward(retain_graph=True)
                 optimizer_fasttrain.step()
 
+                ## ema model update
+                self.model_train_ema.update(fasttrain)
+
                 if i % 100 == 0:
                     self.trainInfoList.append([epoch, i, loss.item()])
                     print('Epoch [{}/{}], Step [{}/{}], LR {:.1e}, Loss: {:.1f}'
                           .format(epoch, self.epochs, str(i).zfill(3), str(self.steps_per_epoch_train).zfill(3),
                                   self.lr, \
                                   loss.item()))
+                    torch.save(fasttrain.state_dict(), os.path.join(
+                    'models/{}/{}/models_train/'.format(self.type, self.model_name), 'model-{}-{}.ckpt'.format(epoch + 1, i + 1)))
             # 
             # print("-----------training loop weights before step-------------\n")
             # for name, p in self.model_train.named_parameters():
                 # /print(p)
                 # break
-                    
-            self.optimizer_train.zero_grad()
-            self.model_train.load_state_dict(fasttrain.state_dict())
+            with torch.no_grad():        
+                self.optimizer_train.zero_grad()
+                self.model_train.load_state_dict(fasttrain.state_dict())
 
             ## ema model update
-            self.model_train_ema.update(self.model_train)
+            # self.model_train_ema.update(self.model_train)
 
             # print("-----------training loop weights after step-------------\n")
             # for name, p in self.model_train.named_parameters():
                 # print(p)
                 # break
-
+            self.model_train.eval()
             for i_val, (inputData_val, target_val) in enumerate(self.val_loader):
                 inputData_val = inputData_val.cuda()
                 target_val = target_val.cuda()
                 target_val = target_val.max(dim=1)[0]
                 with autocast():
                     output_val = fasttrain(inputData_val).float()
+                    with torch.no_grad():
+                        output_train_val = self.model_train(inputData_val).float()
                 loss_val = self.criteria(output_val, target_val, self.weights)
                 optimizer_fasttrain.zero_grad()
                 loss_val = loss_val.max()
                 loss_val.backward(retain_graph=True)
                 optimizer_fasttrain.step()
 
+                with torch.no_grad():
+                    loss_train_val = self.criteria(output_train_val, target_val, self.weights)
+                    loss_train_val = loss_train_val.sum()
+                    gradients = []
+                    for j, params in enumerate(fasttrain.parameters()):
+                        gradients.append(deepcopy(params.grad))
+
+                    for j, p in enumerate(self.model_val.parameters()):
+                        p.grad = gradients[j]
+
+                    self.optimizer_val.step()
+                    self.optimizer_val.zero_grad()
+
+                ## ema model update
+                self.model_val_ema.update(self.model_val)
+
+                with autocast():
+                    outputs_val = self.model_val(inputData_val).float()
+                with torch.no_grad():
+                    self.weights = torch.sigmoid(outputs_val.mean(dim=0))  ## updating weights
+
                 if i_val % 100 == 0:
                     self.valInfoList.append([epoch, i_val, loss_val.max().item()])
+                    self.modeltrainvalinfo.append([epoch, i_val, loss_train_val.item()])
                     print('Outer loop Epoch [{}/{}], Step [{}/{}], LR {:.1e}, Loss: {:.1f}'
                           .format(epoch, self.epochs, str(i_val).zfill(3), str(self.steps_per_epoch_val).zfill(3),
                                   self.lr, \
                                   loss_val.max().item()))
+                    torch.save(self.model_val.state_dict(), os.path.join(
+                    'models/{}/{}/models_val/'.format(self.type, self.model_name), 'model-{}-{}.ckpt'.format(epoch + 1, i_val + 1)))
+                    torch.save(self.weights, os.path.join(
+                    'models/{}/{}/weights/'.format(self.type, self.model_name), 'weights-{}-{}.ckpt'.format(epoch + 1, i_val + 1)))  
+
+            self.model_train.train()
 
             # print("-----------val loop gradients before-------------")
             # for i,p in enumerate(self.model_val.parameters()):
@@ -306,35 +342,35 @@ class learner(nn.Module):
                 # if i == 2:
                     # break
 
-            gradients = []
-            for i, params in enumerate(fasttrain.parameters()):
-                gradients.append(deepcopy(params.grad))
+            # gradients = []
+            # for i, params in enumerate(fasttrain.parameters()):
+            #     gradients.append(deepcopy(params.grad))
 
-            # print("-----------val loop gradients after-------------",len(gradients))
-            for i, p in enumerate(self.model_val.parameters()):
-                p.grad = gradients[i]
+            # # print("-----------val loop gradients after-------------",len(gradients))
+            # for i, p in enumerate(self.model_val.parameters()):
+            #     p.grad = gradients[i]
 
-            # for i,p in enumerate(self.model_val.parameters()):
-                # print(p.grad)
-                # if i == 2:
-                    # break
+            # # for i,p in enumerate(self.model_val.parameters()):
+            #     # print(p.grad)
+            #     # if i == 2:
+            #         # break
             
-            # print("-----------val loop weights before step-------------\n")
-            # for name, p in self.model_val.named_parameters():
-                # print(p)
-                # break
-            self.optimizer_val.step()
-            self.optimizer_val.zero_grad()
+            # # print("-----------val loop weights before step-------------\n")
+            # # for name, p in self.model_val.named_parameters():
+            #     # print(p)
+            #     # break
+            # self.optimizer_val.step()
+            # self.optimizer_val.zero_grad()
 
-            ## ema model update
-            self.model_val_ema.update(self.model_val)
+            # ## ema model update
+            # self.model_val_ema.update(self.model_val)
             # print("-----------val loop weights after step-------------\n")
             # for name, p in self.model_val.named_parameters():
                 # print(p)
                 # break
-            with autocast():
-                outputs_val = self.model_val(inputData_val).float()
-            self.weights = torch.sigmoid(outputs_val.mean(dim=0))  ## updating weights
+            # with autocast():
+            #     outputs_val = self.model_val(inputData_val).float()
+            # self.weights = torch.sigmoid(outputs_val.mean(dim=0))  ## updating weights
 
             del fasttrain, parameters_fasttrain, optimizer_fasttrain
             torch.cuda.empty_cache()
@@ -342,13 +378,12 @@ class learner(nn.Module):
             gc.collect()
             # torch.cuda.empty_cache()
             ## saving checkpoints
-            if i % 100 == 0:
-                torch.save(self.model_train.state_dict(), os.path.join(
-                    'models/{}/{}/models_train/'.format(self.type, self.model_name), 'model-{}-{}.ckpt'.format(epoch + 1, i + 1)))
-                torch.save(self.model_val.state_dict(), os.path.join(
-                    'models/{}/{}/models_val/'.format(self.type, self.model_name), 'model-{}-{}.ckpt'.format(epoch + 1, i + 1)))
-                torch.save(self.weights, os.path.join(
-                    'models/{}/{}/weights/'.format(self.type, self.model_name), 'weights-{}-{}.ckpt'.format(epoch + 1, i + 1)))  
+            # if i % 100 == 0:
+                
+            #     torch.save(self.model_val.state_dict(), os.path.join(
+            #         'models/{}/{}/models_val/'.format(self.type, self.model_name), 'model-{}-{}.ckpt'.format(epoch + 1, i + 1)))
+            #     torch.save(self.weights, os.path.join(
+            #         'models/{}/{}/weights/'.format(self.type, self.model_name), 'weights-{}-{}.ckpt'.format(epoch + 1, i + 1)))  
 
             ## evaluation of the models
             print("---------evaluating the models---------\n")
@@ -373,8 +408,13 @@ class learner(nn.Module):
             print('current_mAP = {:.2f}, highest_mAP = {:.2f}\n'.format(mAP_score_val, self.highest_mAP_val))
 
         ## printing train and val info lists
+        print("-----------------train info list-----------------")
         for i in range(len(self.trainInfoList)):
             print(self.trainInfoList[i])
+        print("-----------------model train val info list-----------------")
+        for i in range(len(self.modeltrainvalinfo)):
+            print(self.modeltrainvalinfo[i])
+        print("-----------------val info list-----------------")
         for i in range(len(self.valInfoList)):
             print(self.valInfoList[i])
 
